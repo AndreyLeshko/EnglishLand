251f55d25bfba8a1153f93d96ab2674eb2e6d3de
AndreyLeshko <andreyleshko2001@gmail.com>
2022-08-11 00:40:26 +0700

    Add repeat words

--- Study_Project/new_words/urls.py
+++ Study_Project/new_words/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     path('api/words/', views.WordsAPIView.as_view()),
     path('en-ru-text/', views.words_en_ru_text, name='words_en_ru_text'),
     path('en-ru-text-result/', views.words_en_ru_text_result, name='words_en_ru_text_result'),
+    path('echo-en-ru-text/', views.echo_words_en_ru_text, name='echo_words_en_ru_text'),
+    path('echo-en-ru-text-result/', views.echo_words_en_ru_text_result, name='echo_words_en_ru_text_result'),
     path('train-words-1/', views.train_words_1, name='train_words_1'),
     path('train-words-1-result/', views.train_words_1_result, name='train_words_1_result'),
     path('add-words/', views.add_words_to_train, name='add_words_to_train'),
