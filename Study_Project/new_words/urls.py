711985c900c70c49fd21de048c6fdda15c177b7a
AndreyLeshko <andreyleshko2001@gmail.com>
2022-08-11 17:42:53 +0700

    Delete old views and templates (trains for any modes)

--- Study_Project/new_words/urls.py
+++ Study_Project/new_words/urls.py
@@ -7,15 +7,7 @@ app_name = 'words'
 
 urlpatterns = [
     path('api/words/', views.WordsAPIView.as_view()),
-
     path('words-text/<mode>/<how_translate>/', views.words_text, name='words_text'),
     path('words-text-result/<mode>/<how_translate>/', views.words_text_result, name='words_text_result'),
-
-    path('en-ru-text/', views.words_en_ru_text, name='words_en_ru_text'),
-    path('en-ru-text-result/', views.words_en_ru_text_result, name='words_en_ru_text_result'),
-    path('echo-en-ru-text/', views.echo_words_en_ru_text, name='echo_words_en_ru_text'),
-    path('echo-en-ru-text-result/', views.echo_words_en_ru_text_result, name='echo_words_en_ru_text_result'),
-    path('train-words-1/', views.train_words_1, name='train_words_1'),
-    path('train-words-1-result/', views.train_words_1_result, name='train_words_1_result'),
     path('add-words/', views.add_words_to_train, name='add_words_to_train'),
 ]
