fc447271e8a62a8bd11b27c2a1c35fc7146b5927
AndreyLeshko <andreyleshko2001@gmail.com>
2022-08-26 13:29:37 +0700

    Add mode with variants

--- Study_Project/settings.py
+++ Study_Project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'key')
+SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpk')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 if 'DEBUG' in os.environ:
--- new_words/urls.py
+++ new_words/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     path('api/words/', views.WordsAPIView.as_view()),
     path('words-text/<mode>/<how_translate>/', views.words_text, name='words_text'),
     path('words-text-result/<mode>/<how_translate>/', views.words_text_result, name='words_text_result'),
+    path('words-with-variants/<mode>/<how_translate>/', views.words_with_variants, name='words_with_variants'),
     path('add-words/', views.add_words_to_train, name='add_words_to_train'),
 ]
