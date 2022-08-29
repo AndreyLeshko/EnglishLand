9a5964f1823b8dd93f9e8568a5a05b9041448c47
AndreyLeshko <andreyleshko2001@gmail.com>
2022-08-29 12:53:41 +0700

    Add section_in_development and photo to edit_profile_page

--- Study_Project/settings.py
+++ Study_Project/settings.py
@@ -144,12 +144,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-# STATICFILES_DIRS = [BASE_DIR/'static']
+# STATICFILES_DIRS = [BASE_DIR/'']
 
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
 
 # Default primary key field type
